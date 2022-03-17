3a884b4b4109bf6261ce9bee2cd92408b3c73ba8
ahmadnurfauziii <1941720068@student.polinema.ac.id>
2022-03-17 10:03:07 +0700

    Tugas - Tambah Mahasiswa.jsx

---
+++ stateless-statefull/src/component/PostMahasiswa/Mahasiswa.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Mahasiswa = (props) => {
+    return (
+        <div className="mahasiswa">
+            <div className="gambar-Mahasiswa">
+                <img src="http://placeimg.com/80/80/mit" alt="Gambar Tumbnail Mahasiswa"/>
+        </div>
+    )
+}
