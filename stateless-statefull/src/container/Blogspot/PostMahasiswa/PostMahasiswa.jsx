429c90fb1d65cfad0ac9042c0f3d04bbec9c3e4c
ahmadnurfauziii <1941720068@student.polinema.ac.id>
2022-03-17 14:16:28 +0700

    Tugas - Update PostMahasiswa

--- stateless-statefull/src/container/Blogspot/PostMahasiswa/PostMahasiswa.jsx
+++ stateless-statefull/src/container/Blogspot/PostMahasiswa/PostMahasiswa.jsx
@@ -59,4 +59,50 @@ class PostMahasiswa extends Component{
                 this.ambilDataDariServerAPI()
             })
     }
-}
+
+    render(){
+        return(
+            <div className="post-mahasiswa">
+                <div className="form pb-2 border-bottom">
+                    <div className="form-group-row">
+                        <label htmlFor="nim" className="col-sm-2 col-form-label">NIM</label>
+                        <div className="col-sm-10">
+                            <input type="text" className="form-control" id="nim" name="nim" onChange={this.handleTambahMahasiswa} />
+                        </div>
+                    </div>
+                    <div className="form-group-row">
+                        <label htmlFor="nama" className="col-sm-2 col-form-label">Nama</label>
+                        <div className="col-sm-10">
+                            <textarea className="form-control" id="nama" name="nama" rows="1" onChange={this.handleTambahMahasiswa}></textarea>
+                        </div>
+                    </div>
+                    <div className="form-group-row">
+                        <label htmlFor="alamat" className="col-sm-2 col-form-label">Alamat</label>
+                        <div className="col-sm-10">
+                            <textarea className="form-control" id="alamat" name="alamat" rows="1" onChange={this.handleTambahMahasiswa}></textarea>
+                        </div>
+                    </div>
+                    <div className="form-group-row">
+                        <label htmlFor="hp" className="col-sm-2 col-form-label">No. HP</label>
+                        <div className="col-sm-10">
+                            <textarea className="form-control" id="hp" name="hp" rows="1" onChange={this.handleTambahMahasiswa}></textarea>
+                        </div>
+                    </div>
+                    <div className="form-group-row">
+                        <label htmlFor="angkatan" className="col-sm-2 col-form-label">Tahun Angkatan</label>
+                        <div className="col-sm-10">
+                            <textarea className="form-control" id="angkatan" name="angkatan" rows="1" onChange={this.handleTambahMahasiswa}></textarea>
+                        </div>
+                    </div>
+                    <div className="form-group-row">
+                        <label htmlFor="status" className="col-sm-2 col-form-label">Status Mahasiswa</label>
+                        <div className="col-sm-10">
+                            <textarea className="form-control" id="status" name="status" rows="1" onChange={this.handleTambahMahasiswa}></textarea>
+                        </div>
+                    </div>
+
+                    <button type="submit" className="btn btn-primary mt-3" onClick={this.handleTombolSimpan}>Simpan</button>
+                </div>
+
+                )
+        }
