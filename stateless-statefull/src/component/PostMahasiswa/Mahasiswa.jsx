32780124d767305bac63c50b5e02af5bdcfbd9f8
ahmadnurfauziii <1941720068@student.polinema.ac.id>
2022-03-17 10:24:44 +0700

    Tugas 1 - Update Mahasiswa.jsx

--- stateless-statefull/src/component/PostMahasiswa/Mahasiswa.jsx
+++ stateless-statefull/src/component/PostMahasiswa/Mahasiswa.jsx
@@ -6,5 +6,15 @@ const Mahasiswa = (props) => {
             <div className="gambar-Mahasiswa">
                 <img src="http://placeimg.com/80/80/mit" alt="Gambar Tumbnail Mahasiswa"/>
         </div>
+        <div className="konten-mahasiswa">
+                <div className="nim-mahasiswa">{props.nim}</div>
+                    <p className="nama-mahasiswa">{props.nama}</p>
+                    <p className="alamat-mahasiswa">{props.alamat}</p>
+                    <p className="hp-mahasiswa">{props.hp}</p>
+                    <p className="angkatan-mahasiswa">{props.angkatan}</p>
+                    <p className="status-mahasiswa">{props.status}</p>
+                    <button className="btn btn-sm btn-warning" onClick={() => props.hapusMahasiswa(props.idMahasiswa)}>Hapus</button>
+            </div>
+        </div>
     )
 }
